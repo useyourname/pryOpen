5b6df6ed3d4dc4a6b6444d387c28307105f3c304
OpenShift guest <rhuq36@gmail.com>
2014-01-24 10:03:59 -0500

    cleanup

--- index.js
+++
@@ -1,6 +0,0 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
-var handle = {"/":requestHandlers.start, "/start":requestHandlers.start};
-
-server.start(router.route, handle);
