e9ba2467923ba692b078448626252fe39054a2da
useyourname <rhuq36@gmail.com>
2014-01-23 21:52:10 -0500

    redesign

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ pryOpen
 ======
 
 doing some node.js in this repo.
+
+https://github.com/clocklimited/BasicExpressSite/blob/master/app.js
+http://clock.co.uk/tech-blogs/a-simple-website-in-nodejs-with-express-jade-and-stylus
--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-var express = require("express");
+var express = require('express');
+var stylus = require('stylus');
 var app = express();
-// app.use(express.static(__dirname));
+
+app.use(stylus.middleware(
+  { 
+  	src: __dirname + '/views/stylesheets/',
+  	dest: __dirname + '/public/css/'
+  }
+));
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 app.get("/", function(request, response){
-	response.send("pryOpen");
+	response.render("index");
 });
-app.listen(process.env.PORT || 8888);
+app.listen(process.env.PORT || 8888, process.env.ID || '127.0.0.1');
 console.log("express server listening on 8888");
