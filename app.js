804ba1caebd313be970dc1d7948a0795957107ee
useyourname <rhuq36@gmail.com>
2014-01-26 13:35:44 -0500

    checks for .apk extension'

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.get("/", function(request, response){
+app.use(function(request, response){
 	response.render("index");
 });
 // app.listen(process.env.PORT || 8888, process.env.ID || '127.0.0.1');
-app.listen(process.env.PORT, process.env.ID);
-console.log("express server listening on 8888");
+app.listen(8080, process.env.ID);
+console.log("express server listening on 8080");
--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -1,9 +1,20 @@
-$('#btn-circle').click(function(){
-  alert('div'); 
-  $('input').click();
-});
+$("document").ready(function(){	
+	$('#btn-circle').click(function(){
+	  alert('div'); 
+	  $('input').click();
+	});
 
-$('input').click(function(event){
-    event.stopPropagation();
-});
+	$('input').click(function(event){
+	  event.stopPropagation();
+	});
 
+	$('input').change(function() {
+  	alert('changed!');
+		var fileName = this.value.toUpperCase();
+	  suffix = ".APK";
+	  if( (fileName.length < 3) || (fileName.substr(fileName.length - 4) !== suffix) ){
+	  	alert('File type not allowed, please upload an APK file');
+			this.value = '';
+	  }
+	});//end of input onChange
+});//end of document ready
