6561e3f96aa98013e3e3c006d0ce05977120d5e5
useyourname <rhuq36@gmail.com>
2014-02-09 20:12:25 -0500

    app.post works

--- app.js
+++ app.js
@@ -10,15 +10,29 @@ app.use(stylus.middleware(
   }
 ));
 app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser({
+	uploadDir: './conversion',
+	defer: true
+}));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.get('/', function(request, response){
 	response.render("index");
 });
+
 app.post('/', function(request, response){
-	alert('post');
-});
+  // request.form.on('progress', function(bytesReceived, bytesExpected){
+	 //  var percent = (bytesReceived / bytesExpected * 100) | 0;
+	 //  console.log('Uploading: %' + percent + '\r');
+  // });
+
+  // request.form.on('end', function() {
+  // 	console.log('UPLOAD COMPLETE');
+  // });
+  
+});//end of app.post()
+
 // app.listen(process.env.PORT || 8888, process.env.ID || '127.0.0.1');
 app.listen(8080, process.env.ID);
 console.log("express server listening on 8080");
--- public/js/upload.js
+++ public/js/upload.js
@@ -15,10 +15,16 @@ $("document").ready(function(){
 
 	$('input').change(function() {
 		var fileName = this.value.toUpperCase();
-	  suffix = ".APK";
-	  if( (fileName.length < 4) || (fileName.substr(fileName.length - 4) !== suffix) ){
+	  var SUFFIX = ".APK";
+	  var MAXSIZE = 52428800;
+	  if( (fileName.length < 4) || (fileName.substr(fileName.length - 4) !== SUFFIX)){
 	  	alert('File type not allowed, please upload an APK file');
 			this.value = '';
 	  }
+	  if(this.files[0].size > MAXSIZE){
+	  	alert('Please upload an APK file smaller than 50MB');
+			this.value = '';
+	  } 
+	  $('form').submit();
 	});//end of input onChange
 });//end of document ready
